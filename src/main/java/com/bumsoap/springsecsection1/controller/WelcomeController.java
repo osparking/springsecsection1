81dc2a6fb43441ced231bb289e9e6e81550bc53e
osparking <jbpark03@gmail.com>
2024-07-28 20:55:14 +0900

    단순 메시지 반환 제어기 메소드

---
+++ src/main/java/com/bumsoap/springsecsection1/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.bumsoap.springsecsection1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome() {
+        return "보안 없는 봄 응용에 환영합니다.";
+    }
+}
