836ed1db0237f12f9d09e2012c79470fbb2566fb
osparking <jbpark03@gmail.com>
2024-07-30 15:30:19 +0900

    package name fixed by removing non existing item

--- src/main/java/com/bumsoap/EasyBankBackendApplication.java
+++ src/main/java/com/bumsoap/EasyBankBackendApplication.java
@@ -1,4 +1,4 @@
-package com.bumsoap.springsecsection1;
+package com.bumsoap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
