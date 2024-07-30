ca76ab93adc07f0832db39599845bfbc308e412a
osparking <jbpark03@gmail.com>
2024-07-30 14:42:02 +0900

    section dependent package name item removed

--- src/main/java/com/bumsoap/controller/WelcomeController.java
+++ src/main/java/com/bumsoap/controller/WelcomeController.java
@@ -1,4 +1,4 @@
-package com.bumsoap.springsecsection1.controller;
+package com.bumsoap.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
