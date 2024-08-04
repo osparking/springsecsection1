0f77b94158806572a7cf3ce983af21d26a1e4174
osparking <jbpark03@gmail.com>
2024-08-04 16:35:34 +0900

    lombok annotation for short source code

--- src/main/java/com/bumsoap/model/Customer.java
+++ src/main/java/com/bumsoap/model/Customer.java
@@ -1,9 +1,12 @@
 package com.bumsoap.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "customer")
+@Getter @Setter
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
