49af79246830b73f02a1c1acae1fad590e828f0c
osparking <jbpark03@gmail.com>
2024-08-04 19:30:54 +0900

    service bean class with load... method

--- src/main/java/com/bumsoap/config/BumsoapUserDetailsService.java
+++ src/main/java/com/bumsoap/config/BumsoapUserDetailsService.java
@@ -1,6 +1,19 @@
 package com.bumsoap.config;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BumsoapUserDetailsService  implements UserDetailsService {
+    /**
+     * @param username the username identifying the user whose data is required.
+     * @return
+     * @throws UsernameNotFoundException
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
