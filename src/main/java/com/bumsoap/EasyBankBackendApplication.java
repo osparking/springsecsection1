7c6489f91e4eddb5ca613e1d4f8b1d16e6e05745
osparking <jbpark03@gmail.com>
2024-08-04 18:00:28 +0900

    optional annocation used for education

--- src/main/java/com/bumsoap/EasyBankBackendApplication.java
+++ src/main/java/com/bumsoap/EasyBankBackendApplication.java
@@ -2,8 +2,12 @@ package com.bumsoap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.bumsoap.repository")
+@EntityScan("com.bumsoap.model")
 public class EasyBankBackendApplication {
 
 	public static void main(String[] args) {
