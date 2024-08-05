78d681960e7ad2bdaf81dece6786907e2ffc755c
osparking <jbpark03@gmail.com>
2024-08-05 11:59:21 +0900

    customer creation internal error handled

--- src/main/java/com/bumsoap/controller/UserController.java
+++ src/main/java/com/bumsoap/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.bumsoap.controller;
 
 import com.bumsoap.model.Customer;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     public ResponseEntity<String> createUser(@RequestBody Customer customer) {
+        try {
+
+        } catch (Exception ex) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("고객 생성 예외: " + ex.getMessage());
+        }
         return null;
     }
 }
