569cd9d17b1e2b99b24413785e0a0738abf1966b
osparking <jbpark03@gmail.com>
2024-08-05 12:04:08 +0900

    private fields for customer creation

--- src/main/java/com/bumsoap/controller/UserController.java
+++ src/main/java/com/bumsoap/controller/UserController.java
@@ -1,13 +1,20 @@
 package com.bumsoap.controller;
 
 import com.bumsoap.model.Customer;
+import com.bumsoap.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class UserController {
+
+    private final CustomerRepository customerRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public ResponseEntity<String> createUser(@RequestBody Customer customer) {
         try {
