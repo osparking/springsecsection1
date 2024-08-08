b8b603595fb8ae7a0b2e2011c0c837766bc5852e
osparking <jbpark03@gmail.com>
2024-08-08 11:56:10 +0900

    our authen provider compoment implements default authenProvider

--- src/main/java/com/bumsoap/config/BumsoapUserPassAuthenProvider.java
+++ src/main/java/com/bumsoap/config/BumsoapUserPassAuthenProvider.java
@@ -1,4 +1,28 @@
 package com.bumsoap.config;
 
-public class BumsoapUserPassAuthenProvider {
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BumsoapUserPassAuthenProvider implements AuthenticationProvider {
+    /**
+     * @param authentication the authentication request object.
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return null;
+    }
+
+    /**
+     * @param authentication
+     * @return
+     */
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return false;
+    }
 }
