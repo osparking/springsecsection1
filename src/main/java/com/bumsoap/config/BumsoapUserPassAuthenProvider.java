2e7c9514cd7ac09b435a2ddebf203de56e58950c
osparking <jbpark03@gmail.com>
2024-08-08 17:02:20 +0900

    using passwordEncoder, check if password matches

--- src/main/java/com/bumsoap/config/BumsoapUserPassAuthenProvider.java
+++ src/main/java/com/bumsoap/config/BumsoapUserPassAuthenProvider.java
@@ -2,10 +2,12 @@ package com.bumsoap.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,18 +15,23 @@ import org.springframework.stereotype.Component;
 public class BumsoapUserPassAuthenProvider implements AuthenticationProvider {
 
     private final BumsoapUserDetailsService bumsoapUserDetailsService;
+    private final PasswordEncoder passwordEncoder;
 
     /**
      * @param authentication the authentication request object.
      * @return
-     * @throws AuthenticationException
+     * @throws BadCredentialsException
      */
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String username = authentication.getName();
         String plainPwd =  authentication.getCredentials().toString();
         UserDetails userDetails = bumsoapUserDetailsService.loadUserByUsername(username);
-        return null;
+        if (passwordEncoder.matches(plainPwd, userDetails.getPassword())) {
+            return new UsernamePasswordAuthenticationToken(username, plainPwd, userDetails.getAuthorities());
+        } else {
+            throw new BadCredentialsException("자격정보 오류");
+        }
     }
 
     /**
