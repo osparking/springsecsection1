eebf7afc89bf0952e818765dc1adabb4fdc446ea
osparking <jbpark03@gmail.com>
2024-08-08 16:47:55 +0900

    acquire three base information for authen

--- src/main/java/com/bumsoap/config/BumsoapUserPassAuthenProvider.java
+++ src/main/java/com/bumsoap/config/BumsoapUserPassAuthenProvider.java
@@ -1,13 +1,19 @@
 package com.bumsoap.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class BumsoapUserPassAuthenProvider implements AuthenticationProvider {
+
+    private final BumsoapUserDetailsService bumsoapUserDetailsService;
+
     /**
      * @param authentication the authentication request object.
      * @return
@@ -15,6 +21,9 @@ public class BumsoapUserPassAuthenProvider implements AuthenticationProvider {
      */
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String plainPwd =  authentication.getCredentials().toString();
+        UserDetails userDetails = bumsoapUserDetailsService.loadUserByUsername(username);
         return null;
     }
 
