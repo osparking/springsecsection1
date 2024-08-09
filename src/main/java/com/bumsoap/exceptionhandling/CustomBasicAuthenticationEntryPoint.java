c84a1e17fdf2cad799ec38c9fbb9c5e2203c405b
osparking <jbpark03@gmail.com>
2024-08-09 16:33:17 +0900

    define 3 local strings for response body items

--- src/main/java/com/bumsoap/exceptionhandling/CustomBasicAuthenticationEntryPoint.java
+++ src/main/java/com/bumsoap/exceptionhandling/CustomBasicAuthenticationEntryPoint.java
@@ -8,11 +8,17 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 public class CustomBasicAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException) throws IOException, ServletException {
+        LocalDateTime currentTimeStamp = LocalDateTime.now();
+        String message = (authException != null && authException.getMessage() != null)
+                ? authException.getMessage() : "Unauthorized";
+        String path = request.getRequestURI();
+
         response.setHeader("Bumsoap-error-reason", "User authentication error");
         response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
     }
