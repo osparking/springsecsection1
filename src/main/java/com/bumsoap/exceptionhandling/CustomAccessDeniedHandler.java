82e4202e13d9f6832857609f94249ee43b87302a
osparking <jbpark03@gmail.com>
2024-08-10 08:54:41 +0900

    맞춤 AccessDeniedHandler - phase 1 - compile error

---
+++ src/main/java/com/bumsoap/exceptionhandling/CustomAccessDeniedHandler.java
@@ -0,0 +1,33 @@
+package com.bumsoap.exceptionhandling;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        LocalDateTime currentTimeStamp = LocalDateTime.now();
+        String message = (authException != null && authException.getMessage() != null)
+                ? authException.getMessage() : "Unauthorized";
+        String path = request.getRequestURI();
+
+        response.setHeader("Bumsoap-error-reason", "User authentication error");
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+
+        response.setContentType("application/json;charset=UTF-8");
+        // Construct the JSON response
+        String jsonResponse =
+                String.format("{\"timestamp\": \"%s\", \"status\": %d, \"error\": \"%s\", \"message\": \"%s\", \"path\": \"%s\"}",
+                        currentTimeStamp, HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase(),
+                        message, path);
+        response.getWriter().write(jsonResponse);
+    }
+}
