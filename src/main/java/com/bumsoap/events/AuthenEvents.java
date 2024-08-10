8782daeb6f60b03875396c9ab556c0af5a7fd413
osparking <jbpark03@gmail.com>
2024-08-11 07:22:29 +0900

    Authentication (success/failure) event listener class defined

--- src/main/java/com/bumsoap/events/AuthenEvents.java
+++ src/main/java/com/bumsoap/events/AuthenEvents.java
@@ -1,10 +1,24 @@
 package com.bumsoap.events;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AbstractAuthenticationFailureEvent;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class AuthenEvents {
+    @EventListener
+    public void onSuccess(AuthenticationSuccessEvent successEvent) {
+        log.info("로그인 성공 사용자 유저이름: {}",
+                successEvent.getAuthentication().getName());
+    }
 
+    @EventListener
+    public void onFailure(AbstractAuthenticationFailureEvent failureEvent) {
+        log.info("로그인 실패 사용자 유저이름: {}, 실패 원인: {}",
+                failureEvent.getAuthentication().getName(),
+                failureEvent.getException().getMessage());
+    }
 }
