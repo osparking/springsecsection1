2f100be41be5f16e3059635e61774a356bdb22b2
osparking <jbpark03@gmail.com>
2024-08-13 17:22:16 +0900

    create accounts table

--- src/main/resources/sql/scripts.sql
+++ src/main/resources/sql/scripts.sql
@@ -21,4 +21,14 @@ INSERT INTO `customer` (`name`,`email`,`mobile_number`, `pwd`, `role`,`create_dt
  VALUES ('범이','jbum@bumsoap.space','01023456789',
     '{bcrypt}$2a$12$P8b.dKAy1RGQC2ioIORiUu4lCcDDTFIms8uDDt8PWlxrakfxj2bfi',
     'admin', CURDATE());
-    
+
+CREATE TABLE `accounts` (
+  `customer_id` int NOT NULL,
+  `account_number` bigint NOT NULL,
+  `account_type` varchar(100) NOT NULL,
+  `branch_address` varchar(200) NOT NULL,
+  `create_dt` date DEFAULT NULL,
+  PRIMARY KEY (`account_number`),
+  KEY `customer_id` (`customer_id`),
+  CONSTRAINT `customer_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE CASCADE
+);
