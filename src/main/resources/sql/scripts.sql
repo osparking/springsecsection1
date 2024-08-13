817c49bfc715064450f541c6602fdfcdc90c87ef
osparking <jbpark03@gmail.com>
2024-08-13 17:11:06 +0900

    create customer table

--- src/main/resources/sql/scripts.sql
+++ src/main/resources/sql/scripts.sql
@@ -5,3 +5,14 @@ create database bumsoap;
 drop table authorities;
 drop table users;
 drop table customer;
+
+CREATE TABLE `customer` (
+  `customer_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `mobile_number` varchar(20) NOT NULL,
+  `pwd` varchar(500) NOT NULL,
+  `role` varchar(100) NOT NULL,
+  `create_dt` date DEFAULT NULL,
+  PRIMARY KEY (`customer_id`)
+);
