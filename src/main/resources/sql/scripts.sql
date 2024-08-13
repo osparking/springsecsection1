25ce3737374d473c0d53508db437b9a02f5ebc59
osparking <jbpark03@gmail.com>
2024-08-13 17:30:54 +0900

    create account_transactions table

--- src/main/resources/sql/scripts.sql
+++ src/main/resources/sql/scripts.sql
@@ -35,3 +35,20 @@ CREATE TABLE `accounts` (
 
 INSERT INTO `accounts` (`customer_id`, `account_number`, `account_type`, `branch_address`, `create_dt`)
  VALUES (1, 411576453459, '저축', '미사강변대로 101', CURDATE());
+
+CREATE TABLE `account_transactions` (
+  `transaction_id` varchar(200) NOT NULL,
+  `account_number` bigint NOT NULL,
+  `customer_id` int NOT NULL,
+  `transaction_dt` date NOT NULL,
+  `transaction_summary` varchar(200) NOT NULL,
+  `transaction_type` varchar(100) NOT NULL,
+  `transaction_amt` int NOT NULL,
+  `closing_balance` int NOT NULL,
+  `create_dt` date DEFAULT NULL,
+  PRIMARY KEY (`transaction_id`),
+  KEY `customer_id` (`customer_id`),
+  KEY `account_number` (`account_number`),
+  CONSTRAINT `accounts_ibfk_2` FOREIGN KEY (`account_number`) REFERENCES `accounts` (`account_number`) ON DELETE CASCADE,
+  CONSTRAINT `acct_user_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE CASCADE
+);
