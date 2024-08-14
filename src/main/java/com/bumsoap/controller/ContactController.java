25100c4d28e6029ef30115b83a528a5f415c2a08
osparking <jbpark03@gmail.com>
2024-08-14 13:57:28 +0900

    contact controller uses repository

--- src/main/java/com/bumsoap/controller/ContactController.java
+++ src/main/java/com/bumsoap/controller/ContactController.java
@@ -1,13 +1,31 @@
 package com.bumsoap.controller;
 
+import com.bumsoap.model.Contact;
+import com.bumsoap.repository.ContactRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Date;
+import java.util.Random;
+
 @RestController
+@RequiredArgsConstructor
 public class ContactController {
 
+    private final ContactRepository contactRepository;
+
     @GetMapping("/contact")
-    public String saveContactInquiryDetails() {
-        return "상세한 문의 정보는 DB 에 저장되었다.";
+    public Contact saveContactInquiryDetails(@RequestBody Contact contact) {
+        contact.setContactId(getServiceReqNumber());
+        contact.setCreateDt(new Date(System.currentTimeMillis()));
+        return contactRepository.save(contact);
+    }
+
+    public String getServiceReqNumber() {
+        Random random = new Random();
+        int ranNum = random.nextInt(999999999 - 9999) + 9999;
+        return "SR" + ranNum;
     }
 }
