202f1287958435151c666bd8e9a205cb39e45875
osparking <jbpark03@gmail.com>
2024-08-14 14:51:43 +0900

    customer controller uses repository to get userDetails

--- src/main/java/com/bumsoap/controller/UserController.java
+++ src/main/java/com/bumsoap/controller/UserController.java
@@ -5,12 +5,15 @@ import com.bumsoap.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Date;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -39,5 +42,12 @@ public class UserController {
                     .status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("고객 생성 예외: " + ex.getMessage());
         }
+    }
+
+    @RequestMapping("/user")
+    public Customer getUserDetailsAfterLogin(Authentication authentication) {
+        Optional<Customer> optionalCustomer
+                = customerRepository.findByEmail(authentication.getName());
+        return optionalCustomer.orElse(null);
     }
 }
