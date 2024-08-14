cd52412ada2087c554fa50758ac6f66fccc2e8fd
osparking <jbpark03@gmail.com>
2024-08-14 17:11:08 +0900

    notices table column name fixed

--- src/main/resources/sql/scripts.sql
+++ src/main/resources/sql/scripts.sql
@@ -124,8 +124,8 @@ CREATE TABLE `notice_details` (
   `notice_id` int NOT NULL AUTO_INCREMENT,
   `notice_summary` varchar(200) NOT NULL,
   `notice_details` varchar(500) NOT NULL,
-  `notic_beg_dt` date NOT NULL,
-  `notic_end_dt` date DEFAULT NULL,
+  `notice_beg_dt` date NOT NULL,
+  `notice_end_dt` date DEFAULT NULL,
   `create_dt` date DEFAULT NULL,
   `update_dt` date DEFAULT NULL,
   PRIMARY KEY (`notice_id`)
