1a37a329f0f33202de90a53f66588673a3f3ba78
osparking <jbpark03@gmail.com>
2024-08-14 11:03:06 +0900

    modify customer class to match DB table

--- src/main/java/com/bumsoap/model/Customer.java
+++ src/main/java/com/bumsoap/model/Customer.java
@@ -1,19 +1,32 @@
 package com.bumsoap.model;
 
-import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.sql.Date;
 
 @Entity
 @Table(name = "customer")
 @Getter @Setter
 public class Customer {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
     private long id;
+    private String name;
     private String email;
-    private String pwd;
-    @Column(name = "role")
-    private String role;
-}
+    private String mobileNumber;
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String pwd;
+
+    private String role;
+
+    @JsonIgnore
+    private Date createAt;
+}
