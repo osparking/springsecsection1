738f2a24d2a1a349b749ac1419e90885205ad287
osparking <jbpark03@gmail.com>
2024-08-14 12:36:24 +0900

    notices controller uses repository

--- src/main/java/com/bumsoap/controller/NoticesController.java
+++ src/main/java/com/bumsoap/controller/NoticesController.java
@@ -1,14 +1,31 @@
 package com.bumsoap.controller;
 
+import com.bumsoap.model.Notice;
+import com.bumsoap.repository.NoticeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 @RestController
+@RequiredArgsConstructor
 public class NoticesController {
 
+    private final NoticeRepository noticeRepository;
+
     @GetMapping("/notices")
-    public String getNotices() {
-        return "이것들은 DB에서 읽어온 알림문들이다.";
+    public ResponseEntity<List<Notice>> getNotices() {
+        List<Notice> notices = noticeRepository.findAllActiveNotices();
+        if (notices == null) {
+            return null;
+        } else {
+            return ResponseEntity.ok()
+                    .cacheControl(CacheControl.maxAge(60, TimeUnit.SECONDS))
+                    .body(notices);
+        }
     }
 }
-
