049532885aa98fd22ddbea63ea4fd45d22fc7578
osparking <jbpark03@gmail.com>
2024-08-15 11:04:03 +0900

    cors policy avoidance config set for port 4200 UI app - prod profile

--- src/main/java/com/bumsoap/config/ProjectSecurityProdConfig.java
+++ src/main/java/com/bumsoap/config/ProjectSecurityProdConfig.java
@@ -2,6 +2,7 @@ package com.bumsoap.config;
 
 import com.bumsoap.exceptionhandling.CustomAccessDeniedHandler;
 import com.bumsoap.exceptionhandling.CustomBasicAuthenticationEntryPoint;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,6 +12,10 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -19,7 +24,19 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class ProjectSecurityProdConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.sessionManagement(smc -> smc.invalidSessionUrl("/invalid_session")
+        http.cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                @Override
+                public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                    config.setAllowedMethods(Collections.singletonList("*"));
+                    config.setAllowCredentials(true);
+                    config.setAllowedHeaders(Collections.singletonList("*"));
+                    config.setMaxAge(3600L);
+                    return config;
+                }
+            }))
+            .sessionManagement(smc -> smc.invalidSessionUrl("/invalid_session")
                         .maximumSessions(1).maxSessionsPreventsLogin(true))
             .requiresChannel(rcc -> rcc.anyRequest().requiresSecure())
             .csrf(csrfConfig -> csrfConfig.disable())
