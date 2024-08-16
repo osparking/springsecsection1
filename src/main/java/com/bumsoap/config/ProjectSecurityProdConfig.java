f1ceb275505899dd59b4809a5743654f264d7b65
osparking <jbpark03@gmail.com>
2024-08-16 21:03:26 +0900

    apply the changes on DefaultConfig onto ProdConfig of ProjectSecurity

--- src/main/java/com/bumsoap/config/ProjectSecurityProdConfig.java
+++ src/main/java/com/bumsoap/config/ProjectSecurityProdConfig.java
@@ -8,10 +8,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -24,7 +27,13 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class ProjectSecurityProdConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+        CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler =
+                new CsrfTokenRequestAttributeHandler();
+
+        http.securityContext(contextConfig -> contextConfig.requireExplicitSave(false))
+            .sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(
+                    SessionCreationPolicy.ALWAYS))
+            .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
                 @Override
                 public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                     CorsConfiguration config = new CorsConfiguration();
@@ -36,10 +45,11 @@ public class ProjectSecurityProdConfig {
                     return config;
                 }
             }))
-            .sessionManagement(smc -> smc.invalidSessionUrl("/invalid_session")
-                        .maximumSessions(1).maxSessionsPreventsLogin(true))
+            .csrf(csrfConfig -> csrfConfig
+                .csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
+                .ignoringRequestMatchers("/contact", "/register")
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
             .requiresChannel(rcc -> rcc.anyRequest().requiresSecure())
-            .csrf(csrfConfig -> csrfConfig.disable())
             .authorizeHttpRequests((req) -> req
                 .requestMatchers("/myAccount", "/myBalance", "/myLoans",
                         "/myCards", "/user")
