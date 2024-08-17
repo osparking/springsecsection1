df0af0b41c4ec565e4b6b5a043d091844511547f
osparking <jbpark03@gmail.com>
2024-08-17 15:34:35 +0900

    insert 4 records into authorities table

--- src/main/resources/sql/scripts.sql
+++ src/main/resources/sql/scripts.sql
@@ -166,3 +166,15 @@ CREATE TABLE `authorities` (
   CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`customer_id`)
   	REFERENCES `customer` (`customer_id`)
 );
+
+INSERT INTO `authorities` (`customer_id`, `role`)
+  VALUES (1, 'VIEWACCOUNT');
+
+INSERT INTO `authorities` (`customer_id`, `role`)
+  VALUES (1, 'VIEWCARDS');
+
+INSERT INTO `authorities` (`customer_id`, `role`)
+  VALUES (1, 'VIEWLOANS');
+
+INSERT INTO `authorities` (`customer_id`, `role`)
+  VALUES (1, 'VIEWBALANCE');
