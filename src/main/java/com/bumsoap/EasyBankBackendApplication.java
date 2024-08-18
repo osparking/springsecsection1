af4c212b78e56cd571ce8b91f74211a8c66bb4c3
osparking <jbpark03@gmail.com>
2024-08-19 08:30:32 +0900

    settings to make filter names display on console

--- src/main/java/com/bumsoap/EasyBankBackendApplication.java
+++ src/main/java/com/bumsoap/EasyBankBackendApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.bumsoap.repository")
 @EntityScan("com.bumsoap.model")
+@EnableWebSecurity(debug = true)
 public class EasyBankBackendApplication {
 
 	public static void main(String[] args) {
