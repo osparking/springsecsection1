e645b8f5fb03c3df58866e5ceec29088badd67af
osparking <jbpark03@gmail.com>
2024-08-19 16:43:35 +0900

    extract username:password format credentials

--- src/main/java/com/bumsoap/filter/RequestValidationBeforeFilter.java
+++ src/main/java/com/bumsoap/filter/RequestValidationBeforeFilter.java
@@ -4,8 +4,11 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpHeaders;
+import org.springframework.util.StringUtils;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 public class RequestValidationBeforeFilter implements Filter {
     @Override
@@ -16,6 +19,14 @@ public class RequestValidationBeforeFilter implements Filter {
         String authHeader = req.getHeader(HttpHeaders.AUTHORIZATION);
         if (authHeader != null) {
             authHeader = authHeader.trim();
+            if (StringUtils.startsWithIgnoreCase(authHeader, "Basic ")) {
+                byte[] base64Token = authHeader.substring(6).getBytes(StandardCharsets.UTF_8);
+                byte[] decoded;
+                try {
+                    decoded = Base64.getDecoder().decode(base64Token);
+                    String token = new String(decoded, StandardCharsets.UTF_8);
+                }
+            }
         }
     }
 }
