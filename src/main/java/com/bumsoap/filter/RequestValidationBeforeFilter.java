e1b7adec0dca747152eb9f96c0c1c4d098fd97e3
osparking <jbpark03@gmail.com>
2024-08-19 16:33:30 +0900

    extract 'Authorization' header from the request

--- src/main/java/com/bumsoap/filter/RequestValidationBeforeFilter.java
+++ src/main/java/com/bumsoap/filter/RequestValidationBeforeFilter.java
@@ -1,6 +1,9 @@
 package com.bumsoap.filter;
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
 
 import java.io.IOException;
 
@@ -8,5 +11,11 @@ public class RequestValidationBeforeFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response,
                          FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        String authHeader = req.getHeader(HttpHeaders.AUTHORIZATION);
+        if (authHeader != null) {
+            authHeader = authHeader.trim();
+        }
     }
 }
