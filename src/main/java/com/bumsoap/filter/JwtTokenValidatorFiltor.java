bdbc847aa10f6009fc75928fdb8aca5fe9c0c438
osparking <jbpark03@gmail.com>
2024-08-20 21:21:19 +0900

    JWT token validation is needed for not /user endpoint request

---
+++ src/main/java/com/bumsoap/filter/JwtTokenValidatorFiltor.java
@@ -0,0 +1,30 @@
+package com.bumsoap.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtTokenValidatorFiltor extends OncePerRequestFilter {
+    /**
+     * @param request
+     * @param response
+     * @param filterChain
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        return request.getServletPath().equals("/user");
+    }
+}
