13a5cb77f41dd2455d09f7616d4913ff9192162c
osparking <jbpark03@gmail.com>
2024-08-20 20:34:19 +0900

    jwt token creator filter skeleton

---
+++ src/main/java/com/bumsoap/filter/JwtTokenGeneratorFilter.java
@@ -0,0 +1,25 @@
+package com.bumsoap.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtTokenGeneratorFilter extends OncePerRequestFilter {
+    /**
+     * @param request
+     * @param response
+     * @param filterChain
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+    }
+}
