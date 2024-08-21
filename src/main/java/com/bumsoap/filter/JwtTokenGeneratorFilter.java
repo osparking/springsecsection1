4e37e0a808d4204b3f462d9dc040de74623c2648
osparking <jbpark03@gmail.com>
2024-08-21 19:57:04 +0900

    expiration time reverted to about 8 hours

--- src/main/java/com/bumsoap/filter/JwtTokenGeneratorFilter.java
+++ src/main/java/com/bumsoap/filter/JwtTokenGeneratorFilter.java
@@ -48,7 +48,7 @@ public class JwtTokenGeneratorFilter extends OncePerRequestFilter {
                         .claim("authorities", auth.getAuthorities().stream().map(
                                 GrantedAuthority::getAuthority).collect(Collectors.joining(",")))
                         .issuedAt(new Date())
-                        .expiration(new Date((new Date()).getTime() + 3000))
+                        .expiration(new Date((new Date()).getTime() + 30000000))
                         .signWith(secretKey).compact();
                 response.setHeader(ApplicationConstants.JWT_HEADER, jwt);
             }
