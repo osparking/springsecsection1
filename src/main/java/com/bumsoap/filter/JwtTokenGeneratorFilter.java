bda61a1793838b97cec10368e47c33a548024f5e
osparking <jbpark03@gmail.com>
2024-08-21 09:34:38 +0900

    doFilterInternal - get secret from environment

--- src/main/java/com/bumsoap/filter/JwtTokenGeneratorFilter.java
+++ src/main/java/com/bumsoap/filter/JwtTokenGeneratorFilter.java
@@ -4,6 +4,9 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.env.Environment;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -21,6 +24,15 @@ public class JwtTokenGeneratorFilter extends OncePerRequestFilter {
                                     HttpServletResponse response,
                                     FilterChain filterChain)
             throws ServletException, IOException {
+        /**
+         * 현재 인증된 유저 상세 정보 확보
+         */
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            Environment env = getEnvironment();
+            env.getProperty("JWT_SECRET", "TQxGOsUedLLrgOgdStA2MLq6XbB1FDlq");
+        }
+        filterChain.doFilter(request, response);
     }
 
     @Override
