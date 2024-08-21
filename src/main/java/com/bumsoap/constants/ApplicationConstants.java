963784abb48abf003c636e9ec2d0e5ddecb34ff7
osparking <jbpark03@gmail.com>
2024-08-21 10:33:46 +0900

    header key 'Authorization' as string constant

--- src/main/java/com/bumsoap/constants/ApplicationConstants.java
+++ src/main/java/com/bumsoap/constants/ApplicationConstants.java
@@ -4,4 +4,5 @@ public final class ApplicationConstants {
     public static final String JWT_SECRET = "JWT_SECRET_KEY";
     public static final String JWT_SECRET_DEFAULT_VALUE =
             "TQxGOsUedLLrgOgdStA2MLq6XbB1FDlq";
+    public static final String JWT_HEADER = "Authorization";
 }
