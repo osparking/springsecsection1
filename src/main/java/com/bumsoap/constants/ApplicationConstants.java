eb34dfefdbbe43ae4a6e254627b5f76c3db83d53
osparking <jbpark03@gmail.com>
2024-08-21 09:46:13 +0900

    JWT_SECRET constants class - fields

---
+++ src/main/java/com/bumsoap/constants/ApplicationConstants.java
@@ -0,0 +1,7 @@
+package com.bumsoap.constants;
+
+public final class ApplicationConstants {
+    public static final String JWT_SECRET = "JWT_SECRET_KEY";
+    public static final String JWT_SECRET_DEFAULT_VALUE =
+            "TQxGOsUedLLrgOgdStA2MLq6XbB1FDlq";
+}
