5d2afe431ff6bb8204a80cacc5d61cc0e9fef988
osparking <jbpark03@gmail.com>
2024-08-22 19:05:49 +0900

    requires just logged in user

--- src/main/java/com/bumsoap/config/ProjectSecurityConfig.java
+++ src/main/java/com/bumsoap/config/ProjectSecurityConfig.java
@@ -68,7 +68,7 @@ public class ProjectSecurityConfig {
             .authorizeHttpRequests((req) -> req
                 .requestMatchers("/myAccount").hasRole("USER")
                 .requestMatchers("/myBalance").hasAnyRole("USER", "ADMIN")
-                .requestMatchers("/myLoans").hasRole("USER")
+                .requestMatchers("/myLoans").authenticated()
                 .requestMatchers("/myCards").hasRole("USER")
                 .requestMatchers("/user").authenticated()
                 .requestMatchers("/notices", "/contact", "/error", "/register",
