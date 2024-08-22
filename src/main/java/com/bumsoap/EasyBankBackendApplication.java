7d6e2b6fb537b2f41b8948fe6718bd699ed6ac8d
osparking <jbpark03@gmail.com>
2024-08-22 16:12:27 +0900

    enable method level security on the main class

--- src/main/java/com/bumsoap/EasyBankBackendApplication.java
+++ src/main/java/com/bumsoap/EasyBankBackendApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.bumsoap.repository")
 @EntityScan("com.bumsoap.model")
 @EnableWebSecurity
+@EnableMethodSecurity(jsr250Enabled = true, securedEnabled = true)
 public class EasyBankBackendApplication {
 
 	public static void main(String[] args) {
