d28aaa461766b8d910f3ff49cb0a7a2db11f34bf
osparking <jbpark03@gmail.com>
2024-08-22 20:08:48 +0900

    verify role at the method level

--- src/main/java/com/bumsoap/repository/LoanRepository.java
+++ src/main/java/com/bumsoap/repository/LoanRepository.java
@@ -2,13 +2,15 @@ package com.bumsoap.repository;
 
 import com.bumsoap.model.Loans;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface LoanRepository extends CrudRepository<Loans, Long> {
-	
+
+	@PreAuthorize("hasRole('USER')")
 	List<Loans> findByCustomerIdOrderByStartDtDesc(long customerId);
 
 }
