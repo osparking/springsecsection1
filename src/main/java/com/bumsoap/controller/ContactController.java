ea78ffff853c9ab5a89cc187433503c3298a86d0
osparking <jbpark03@gmail.com>
2024-08-23 19:12:22 +0900

    use PostFilter on contactSave method

--- src/main/java/com/bumsoap/controller/ContactController.java
+++ src/main/java/com/bumsoap/controller/ContactController.java
@@ -3,12 +3,13 @@ package com.bumsoap.controller;
 import com.bumsoap.model.Contact;
 import com.bumsoap.repository.ContactRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreFilter;
+import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -19,16 +20,17 @@ public class ContactController {
     private final ContactRepository contactRepository;
 
     @PostMapping("/contact")
-    @PreFilter("filterObject.contactName != 'Test'")
-    public Contact saveContactInquiryDetails(@RequestBody List<Contact> contacts) {
-        if (contacts.isEmpty())
-            return null;
-        else {
+    @PostFilter("filterObject.contactName != 'Test'")
+    public List<Contact> saveContactInquiryDetails(@RequestBody List<Contact> contacts) {
+        List<Contact> returnContacts = new ArrayList<>();
+        if (!contacts.isEmpty()) {
             Contact contact = contacts.getFirst();
             contact.setContactId(getServiceReqNumber());
             contact.setCreateDt(new Date(System.currentTimeMillis()));
-            return contactRepository.save(contact);
+            Contact savedContact = contactRepository.save(contact);
+            returnContacts.add(savedContact);
         }
+        return returnContacts;
     }
 
     public String getServiceReqNumber() {
