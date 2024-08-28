c90453c4401091dcb36dabd70250f7727e4a9793
osparking <jbpark03@gmail.com>
2024-08-28 16:56:37 +0900

    let LoansController method receive email instead of ID

--- src/main/java/com/bumsoap/controller/LoansController.java
+++ src/main/java/com/bumsoap/controller/LoansController.java
@@ -1,6 +1,8 @@
 package com.bumsoap.controller;
 
+import com.bumsoap.model.Customer;
 import com.bumsoap.model.Loans;
+import com.bumsoap.repository.CustomerRepository;
 import com.bumsoap.repository.LoanRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PostAuthorize;
@@ -9,17 +11,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 public class LoansController {
 
     private final LoanRepository loanRepository;
+    private final CustomerRepository customerRepository;
 
     @GetMapping("/myLoans")
     @PostAuthorize("hasRole('USER')")
-    public List<Loans> getLoanDetails(@RequestParam long id) {
-        List<Loans> loans = loanRepository.findByCustomerIdOrderByStartDtDesc(id);
-        return loans;
+    public List<Loans> getLoanDetails(@RequestParam String email) {
+        Optional<Customer> optionalCustomer = customerRepository.findByEmail(email);
+        
+        if (optionalCustomer.isPresent()) {
+            return loanRepository.findByCustomerIdOrderByStartDtDesc(
+                    optionalCustomer.get().getId());
+        } else {
+            return null;
+        }
     }
 }
