74b5e7aa073b03225ee3f4b7a1302c74c3c20a0b
osparking <jbpark03@gmail.com>
2024-08-28 12:20:03 +0900

    remove request handler for /register endpoint

--- src/main/java/com/bumsoap/controller/UserController.java
+++ src/main/java/com/bumsoap/controller/UserController.java
@@ -4,17 +4,12 @@ import com.bumsoap.model.Customer;
 import com.bumsoap.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.Date;
 import java.util.Optional;
 
 @RestController
@@ -25,28 +20,6 @@ public class UserController {
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
     private final Environment env;
-
-    @PostMapping("/register")
-    public ResponseEntity<String> createUser(@RequestBody Customer customer) {
-        try {
-            String hashedPwd = passwordEncoder.encode(customer.getPwd());
-            customer.setPwd(hashedPwd);
-            customer.setCreateDt(new Date(System.currentTimeMillis()));
-            Customer customerInDB = customerRepository.save(customer);
-
-            if (customerInDB.getId() > 0) {
-                return ResponseEntity.status(HttpStatus.CREATED)
-                        .body("고객 정보가 저장되었습니다.");
-            } else {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                        .body("고객 정보 저장에 실패하였습니다.");
-            }
-        } catch (Exception ex) {
-            return ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("고객 생성 예외: " + ex.getMessage());
-        }
-    }
 
     @RequestMapping("/user")
     public Customer getUserDetailsAfterLogin(Authentication authentication) {
