ea1dc9862668be0e8afc607f8ef7d8e11d2a91ea
osparking <jbpark03@gmail.com>
2024-08-28 12:09:12 +0900

    remove unnecessary security related constants

--- src/main/java/com/bumsoap/constants/ApplicationConstants.java
+++ src/main/java/com/bumsoap/constants/ApplicationConstants.java
@@ -1,8 +1,5 @@
 package com.bumsoap.constants;
 
 public final class ApplicationConstants {
-    public static final String JWT_SECRET = "JWT_SECRET_KEY";
-    public static final String JWT_SECRET_DEFAULT_VALUE =
-            "TQxGOsUedLLrgOgdStA2MLq6XbB1FDlq";
-    public static final String JWT_HEADER = "Authorization";
+
 }
