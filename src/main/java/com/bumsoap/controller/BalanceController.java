f18756f1fec9341d07f107d97c0841fa81a4d4ce
osparking <jbpark03@gmail.com>
2024-08-28 16:35:19 +0900

    let BalanceController method receive email instead of ID

--- src/main/java/com/bumsoap/controller/BalanceController.java
+++ src/main/java/com/bumsoap/controller/BalanceController.java
@@ -1,24 +1,32 @@
 package com.bumsoap.controller;
 
 import com.bumsoap.model.AccountTransactions;
+import com.bumsoap.model.Customer;
 import com.bumsoap.repository.AccountTransactionsRepository;
+import com.bumsoap.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 public class BalanceController {
 
     private final AccountTransactionsRepository accountTransactionsRepository;
+    private final CustomerRepository customerRepository;
 
     @GetMapping("/myBalance")
-    public List<AccountTransactions> getBalanceDetails(@RequestParam long id) {
-        List<AccountTransactions> accountTransactions = accountTransactionsRepository.
-                findByCustomerIdOrderByTransactionDtDesc(id);
-        return accountTransactions;
+    public List<AccountTransactions> getBalanceDetails(@RequestParam String email) {
+        Optional<Customer> optionalCustomer = customerRepository.findByEmail(email);
+        if (optionalCustomer.isPresent()) {
+            List<AccountTransactions> accountTransactions = accountTransactionsRepository.
+                    findByCustomerIdOrderByTransactionDtDesc(optionalCustomer.get().getId());
+            return accountTransactions;
+        }
+        return null;
     }
 }
