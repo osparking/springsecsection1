26a3fc39ba2cf724ae2e009c5231d16eecf1ca6c
osparking <jbpark03@gmail.com>
2024-08-28 12:17:20 +0900

    authN related apiLogin method removed

--- src/main/java/com/bumsoap/controller/UserController.java
+++ src/main/java/com/bumsoap/controller/UserController.java
@@ -1,31 +1,21 @@
 package com.bumsoap.controller;
 
-import com.bumsoap.constants.ApplicationConstants;
 import com.bumsoap.model.Customer;
-import com.bumsoap.model.LoginRequestDto;
-import com.bumsoap.model.LoginResponseDto;
 import com.bumsoap.repository.CustomerRepository;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
 import java.sql.Date;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -65,36 +55,5 @@ public class UserController {
         return optionalCustomer.orElse(null);
     }
     
-    @PostMapping("/apiLogin")
-    public ResponseEntity<LoginResponseDto> apiLogin(
-            @RequestBody LoginRequestDto loginRequestDto) {
-        String jwt = null;
-        Authentication authentication =
-                UsernamePasswordAuthenticationToken.unauthenticated(
-                        loginRequestDto.username(), loginRequestDto.password());
-        Authentication authenticationDone =
-                authenticationManager.authenticate(authentication);
-        if (authenticationDone != null && authenticationDone.isAuthenticated()) {
-            if (env != null) {
-                String secret = env.getProperty(ApplicationConstants.JWT_SECRET,
-                        ApplicationConstants.JWT_SECRET_DEFAULT_VALUE);
-                SecretKey secretKey =
-                        Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
-                /**
-                 * jwt 토큰 생성
-                 */
-                jwt = Jwts.builder().issuer("BumSoap").subject("JWT Token")
-                        .claim("username", authenticationDone.getName())
-                        .claim("authorities", authenticationDone.getAuthorities().stream().map(
-                                GrantedAuthority::getAuthority).collect(Collectors.joining(",")))
-                        .issuedAt(new java.util.Date())
-                        .expiration(new java.util.Date((new java.util.Date()).getTime() + 30000000))
-                        .signWith(secretKey).compact();
-            }
-        }
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .header(ApplicationConstants.JWT_HEADER, jwt)
-                .body(new LoginResponseDto(HttpStatus.OK.getReasonPhrase(), jwt));
-    }
+
 }
