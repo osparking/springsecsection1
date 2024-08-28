0cb02cbe2a0fa00e9178a2b009e12fdbf508b4d0
osparking <jbpark03@gmail.com>
2024-08-28 12:23:19 +0900

    remove unnecessary private fields and imports

--- src/main/java/com/bumsoap/controller/UserController.java
+++ src/main/java/com/bumsoap/controller/UserController.java
@@ -3,10 +3,7 @@ package com.bumsoap.controller;
 import com.bumsoap.model.Customer;
 import com.bumsoap.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.env.Environment;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +14,6 @@ import java.util.Optional;
 public class UserController {
 
     private final CustomerRepository customerRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final AuthenticationManager authenticationManager;
-    private final Environment env;
 
     @RequestMapping("/user")
     public Customer getUserDetailsAfterLogin(Authentication authentication) {
@@ -27,6 +21,5 @@ public class UserController {
                 = customerRepository.findByEmail(authentication.getName());
         return optionalCustomer.orElse(null);
     }
-    
 
 }
