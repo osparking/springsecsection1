70c9c9baa3ba7f924472960cf213c6e1d6c2b8f7
osparking <jbpark03@gmail.com>
2024-08-28 16:50:32 +0900

    let CardsController method receive email instead of ID

--- src/main/java/com/bumsoap/controller/CardsController.java
+++ src/main/java/com/bumsoap/controller/CardsController.java
@@ -1,23 +1,32 @@
 package com.bumsoap.controller;
 
 import com.bumsoap.model.Cards;
+import com.bumsoap.model.Customer;
 import com.bumsoap.repository.CardsRepository;
+import com.bumsoap.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 public class CardsController {
 
     private final CardsRepository cardsRepository;
+    private final CustomerRepository customerRepository;
 
     @GetMapping("/myCards")
-    public List<Cards> getCardDetails(@RequestParam long id) {
-        List<Cards> cards = cardsRepository.findByCustomerId(id);
-        return cards;
+    public List<Cards> getCardDetails(@RequestParam String email) {
+        Optional<Customer> optionalCustomer = customerRepository.findByEmail(email);
+        if (optionalCustomer.isPresent()) {
+            List<Cards> cards = cardsRepository.findByCustomerId(optionalCustomer.get().getId());
+            return cards;
+        } else {
+            return null;
+        }
     }
 }
