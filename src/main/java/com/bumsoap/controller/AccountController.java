108e38246a0fd26dca591b070eacdfc15ad16ca6
osparking <jbpark03@gmail.com>
2024-08-28 16:28:53 +0900

    let request handler method receive email instead of id

--- src/main/java/com/bumsoap/controller/AccountController.java
+++ src/main/java/com/bumsoap/controller/AccountController.java
@@ -1,21 +1,34 @@
 package com.bumsoap.controller;
 
 import com.bumsoap.model.Accounts;
+import com.bumsoap.model.Customer;
 import com.bumsoap.repository.AccountsRepository;
+import com.bumsoap.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 public class AccountController {
 
     private final AccountsRepository accountsRepository;
+    private final CustomerRepository customerRepository;
 
     @GetMapping("/myAccount")
-    public Accounts getAccountDetails(@RequestParam long id) {
-        Accounts accounts = accountsRepository.findByCustomerId(id);
-        return accounts;
+    public Accounts getAccountDetails(@RequestParam String email) {
+        Optional<Customer> optionalCustomer = customerRepository.findByEmail(email);
+        if (optionalCustomer.isPresent()) {
+            Accounts accounts = accountsRepository.findByCustomerId(optionalCustomer.get().getId());
+            if (accounts != null) {
+                return accounts;
+            } else {
+                return null;
+            }
+        }
+        return null;
     }
 }
